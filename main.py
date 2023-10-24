1bde5ae89b24a6f62d5a94e390413a92e07cb904
DeLuks <dragojeviclk16@gmail.com>
2023-10-24 20:42:28 +0200

    added embed message command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 
 # [i] - info
 # [-] - shits going down (error)
@@ -13,12 +14,20 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        if message.content.startswith('!hello'):
+        if message.content.startswith('/hello'):
             await message.reply('Hello!', mention_author = True)
             print("[+] Bot replied to user!!")
+
+        if message.content.startswith('/embed'):
+            embed = discord.Embed(title="Quote Of The Day", description = "submitted by:\n \- user",color=0x8aff97) # // title and other shiz
+            embed.add_field(name="the darker the night, the brighter the stars, the deeper the grief, the closer is god", value = "", inline = True) # // here comes the acutal quote /// value = "" must be there for some reason
+            embed.set_footer(text="- Apollon Maykov") # // put here the authors name
+
+            await message.reply(embed=embed)
+            print("[+] created embed")
 
 intents = discord.Intents.default()
 intents.messages = True
 
 client = MyClient(intents=intents)
-client.run('MTE2NjQyODc0NDE2OTMxMjM0OA.Gqkeie.s1ZIHVCV0kXFfgsp2uXy6KgFfOxEQHBM5bJzog')
+client.run('')
